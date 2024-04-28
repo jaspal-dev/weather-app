66544a92e27bdfe258c780356893e4d74c93f15e
Jaspal Singh <135453532+jaspal-dev@users.noreply.github.com>
2024-04-28 15:45:04 +0530

    Feature/setup husky (#10)
    * Added linting before pushing code
    
    * Updated husky message

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,6 +40,12 @@ export default [
     },
   },
   ...compat.extends('standard'),
-  pluginReactConfig,
+  Object.assign(pluginReactConfig, {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  }),
   eslintPluginPrettierRecommended,
 ];
