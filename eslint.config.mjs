24d4d97a2f466f3d920d6eecd6b95789454016ae
Jaspal Singh <contact@jaspal.dev>
2024-04-28 05:17:51 +0000

    Added configurations for eslint prettier plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,23 @@
-import globals from 'globals'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import pluginJs from '@eslint/js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
   { languageOptions: { globals: globals.browser } },
   ...compat.extends('standard'),
-  pluginReactConfig
-]
+  pluginReactConfig,
+  eslintPluginPrettierRecommended,
+];
