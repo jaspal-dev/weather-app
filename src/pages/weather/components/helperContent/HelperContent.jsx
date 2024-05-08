d3427e8165a54f0b4b2a71c9bdbddb6766c1819a
Jaspal Singh <135453532+jaspal-dev@users.noreply.github.com>
2024-05-08 20:07:57 +0530

    feat: added loading screen (#34)

--- src/pages/weather/components/helperContent/HelperContent.jsx
+++ src/pages/weather/components/helperContent/HelperContent.jsx
@@ -1,26 +1,57 @@
 import { Box, Stack, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Lottie from 'react-lottie';
 
-const HelperContent = ({ description, heading }) => {
+import * as loadingAnimation from './../../../../assets/animations/LoadingAnimation.json';
+import { constants } from './../../../../constants';
+import { contents } from './../../contents';
+
+const { LOADING_STATUS } = constants;
+
+const GenerateHelperContent = ({ isError }) => {
+  return isError ? (
+    <Box>
+      <Typography variant={'h4'}>{contents.errorHeadingText}</Typography>
+      <Typography variant={'h6'}>{contents.errorHeadingDescription}</Typography>
+    </Box>
+  ) : (
+    <Box>
+      <Typography variant={'h4'}>{contents.startSearching}</Typography>
+      <Typography variant={'h6'}>{contents.searchHelp}</Typography>
+    </Box>
+  );
+};
+
+const HelperContent = ({ callbackFnInfo }) => {
   return (
     <Stack
-      height={150}
+      height={300}
       justifyContent={'center'}
       marginY={5}
       textAlign={'center'}
     >
-      <Box>
-        <Typography variant={'h4'}>{heading}</Typography>
-        <Typography variant={'h6'}>{description}</Typography>
-      </Box>
+      {callbackFnInfo.status === LOADING_STATUS.LOADING ? (
+        <Box height={300}>
+          <Lottie options={{ animationData: loadingAnimation }} />
+        </Box>
+      ) : (
+        <GenerateHelperContent isError={callbackFnInfo.error !== null} />
+      )}
     </Stack>
   );
 };
 
+GenerateHelperContent.propTypes = {
+  isError: PropTypes.bool,
+};
+
 HelperContent.propTypes = {
-  description: PropTypes.string.isRequired,
-  heading: PropTypes.string.isRequired,
+  callbackFnInfo: PropTypes.shape({
+    error: PropTypes.object,
+    response: PropTypes.object,
+    status: PropTypes.string,
+  }).isRequired,
 };
 
 export { HelperContent };
