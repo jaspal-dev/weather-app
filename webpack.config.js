74dbb4225b5df363df48382060b6d91a25aa4dcf
Jaspal Singh <contact@jaspal.dev>
2024-05-09 20:52:24 +0000

    feat: added env support

--- webpack.config.js
+++ webpack.config.js
@@ -4,76 +4,80 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  devServer: {
-    hot: true,
-    port: 9000,
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-  },
-  entry: './src/index.js',
-  mode: 'none',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+module.exports = (env) => {
+  require('dotenv').config({ path: `./.env.${env.NODE_ENV}` });
+  return {
+    devServer: {
+      hot: true,
+      port: 9000,
+      static: {
+        directory: path.join(__dirname, 'public'),
       },
-      {
-        exclude: /node_modules/,
-        resolve: {
-          extensions: ['', '.js', '.jsx'],
+    },
+    entry: './src/index.js',
+    mode: 'none',
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-        test: /\.(?:js|jsx|mjs|cjs)$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              [
-                '@emotion',
-                {
-                  importMap: {
-                    '@mui/material/styles': {
-                      styled: {
-                        canonicalImport: ['@emotion/styled', 'default'],
-                        styledBaseImport: ['@mui/material/styles', 'styled'],
+        {
+          exclude: /node_modules/,
+          resolve: {
+            extensions: ['', '.js', '.jsx'],
+          },
+          test: /\.(?:js|jsx|mjs|cjs)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                [
+                  '@emotion',
+                  {
+                    importMap: {
+                      '@mui/material/styles': {
+                        styled: {
+                          canonicalImport: ['@emotion/styled', 'default'],
+                          styledBaseImport: ['@mui/material/styles', 'styled'],
+                        },
                       },
-                    },
-                    '@mui/system': {
-                      styled: {
-                        canonicalImport: ['@emotion/styled', 'default'],
-                        styledBaseImport: ['@mui/system', 'styled'],
+                      '@mui/system': {
+                        styled: {
+                          canonicalImport: ['@emotion/styled', 'default'],
+                          styledBaseImport: ['@mui/system', 'styled'],
+                        },
                       },
                     },
                   },
-                },
+                ],
               ],
-            ],
-            presets: [
-              ['@babel/preset-env', { targets: 'defaults' }],
-              ['@babel/preset-react'],
-            ],
+              presets: [
+                ['@babel/preset-env', { targets: 'defaults' }],
+                ['@babel/preset-react'],
+              ],
+            },
           },
         },
-      },
+      ],
+    },
+    output: {
+      clean: true,
+      filename: 'bundle.[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.ejs',
+        title: 'Webpack App',
+      }),
+      new MiniCssExtractPlugin(),
+      // fix "process is not defined" error:
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+      new webpack.EnvironmentPlugin(Object.keys(process.env)),
     ],
-  },
-  output: {
-    clean: true,
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.ejs',
-      title: 'Webpack App',
-    }),
-    new MiniCssExtractPlugin(),
-    // fix "process is not defined" error:
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-  ],
+  };
 };
