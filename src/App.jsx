d4432f69259da529a0205866c73a1c088bc98a3b
Jaspal Singh <contact@jaspal.dev>
2024-05-17 19:02:04 +0000

    test: test-content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-
-import { Weather } from './pages/weather';
+import React, { useState } from 'react';
 
 export default function App() {
-  return <Weather />;
+  const [counter, setCounter] = useState(0);
+  return (
+    <>
+      {/* {undefined.a} */}
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+    </>
+  );
 }
